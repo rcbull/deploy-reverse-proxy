e92aeb27b1d0e266a3892cfaba95599633e6baec
rcbull <rcbull@gmail.com>
2021-01-13 11:37:27 -0300

    updated domain to add .com

--- teste.sh
+++ teste.sh
@@ -1,13 +1,13 @@
 
 # sudo docker network create net
 
-sudo docker-compose up -d
+# sudo docker-compose up -d
 
 cd domain1
 
 sudo docker build . -t nginx1
 
-sudo docker run -dt --name domain1 -e VIRTUAL_HOST=domain1 -e LETSENCRYPT_HOST="domain1" --network net nginx1:latest
+sudo docker run -dt --name domain1 -e VIRTUAL_HOST=domain1.com -e LETSENCRYPT_HOST="domain1.com" --network net nginx1:latest
 
 cd ..
 
@@ -15,6 +15,6 @@ cd domain2
 
 sudo docker build . -t nginx2
 
-sudo docker run -dt --name domain2 -e VIRTUAL_HOST=domain2 -e LETSENCRYPT_HOST="domain2" --network net nginx2:latest
+sudo docker run -dt --name domain2 -e VIRTUAL_HOST=domain2.com -e LETSENCRYPT_HOST="domain2.com" --network net nginx2:latest
 
 cd ..
